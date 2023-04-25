5589778719c7a1dbad1c551ca5b41bca15dcd4b0
muminovva <muminovvaumida@gmail.com>
2023-04-25 17:06:57 +0500

    figma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const[count, setCount] = useState(0)
+   if(count>99){
+    return setCount (0)
+   }
 
+  return(
+    <>
+    
+    <div className='big'>
+    <button style={{justifyContent:"center"}} className='btn1'>{count}</button> 
+    </div>
+    
+   <div className='fd'>
+   <button className='btn' onClick={() => setCount(count+1)}>Add</button>
+   </div>
+
+    </>
+  )
+  }
 export default App;
