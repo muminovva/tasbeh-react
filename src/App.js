883ea6cf2627844ea54a5675738d69a82d7a2608
muminovva <muminovvaumida@gmail.com>
2023-04-27 14:57:08 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import { useState } from 'react';
 import './App.css';
+import audio from "./img/ping-82822.mp3";
 
 function App() {
-  const[count, setCount] = useState(0)
-   if(count>99){
-    return setCount (0)
-   }
 
-  return(
+  const [count, setCount] = useState(0)
+  if (count > 99) {
+    return setCount(0)
+  }
+  
+  function playw() {
+    setCount(count + 1)
+    new Audio(audio).play()
+  }
+  function resert() {
+    setCount(0)
+  }
+  return (
     <>
-    
-    <div className='big'>
-    <button style={{justifyContent:"center"}} className='btn1'>{count}</button> 
-    </div>
-    
-   <div className='fd'>
-   <button className='btn' onClick={() => setCount(count+1)}>Add</button>
-   </div>
 
+      <header>
+        <audio src={audio}></audio>
+        <div className='big'>
+          <button style={{ justifyContent: "center" }} className='btn1'>{count}/{99 - count} <h6 className='h6'>مسبحة</h6></button>
+        </div>
+
+        <div className='fd'>
+          <button className='btn' onClick={() => playw()} >Add</button>
+          <button id='but' className='btn ' onClick={() => (resert)}>Resert</button>
+
+        </div>
+
+      </header>
     </>
   )
-  }
+}
 export default App;
